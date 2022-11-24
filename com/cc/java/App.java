71f79deffa3f31b5cef9567c875ed7d9fd13ae87
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-24 11:41:16 +0100

    mehrer Beispiele

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -2,32 +2,44 @@ package com.cc.java;
 
 public class App {
     
-    public static void main(String[] args) {
+    public static void main( String[] args ) {
     
-        Cat cat = new Cat("Grizabella","white",29);
-        output(cat.getName());
-        output(cat.getFurColor());
-        output(String.valueOf(cat.getAge()));
-    
-        output("------------------------");
+        // Cat cat = new Cat( "Rose", "white", 4, true );
+        //     output( cat.getStringAttributes( "#name" ) );
+        //     output( cat.getStringAttributes( "#furColor" ) );
+        //     output( cat.getStringAttributes( "#age2" ) );                                   // getter Ausgabe aus der Kombination mit flag
+        //     output( "------------------------" );
 
-       Cat cat1 = new Cat("Alonzo", "grey", 35);
+        // Cat cat1 = new Cat( "Beaty", "black", 7, true );
+        //     output( cat1.getStringAttributes( "#name" ) );
+        //     output( cat1.getStringAttributes( "#furColor" ) );
+        //     output( String.valueOf( cat1.getAge() ) );                                          // getter Ausgabe des int age
+        //     output( "------------------------" );
 
-       cat1.setFurColor("striped");
+        // Cat cat2 = new Cat( "Steve", "grey", 9, false );
+        //     output( cat2.getStringAttributes( "#name" ) );
+        //     output( cat2.getStringAttributes( "#furColor" ) );
+        //     output( cat2.getAge() );                                                            // getter Ausgabe des String age
+        //     output( "------------------------" );
 
-        output(cat1.getName());
-        output(cat1.getFurColor());
-        output(String.valueOf(cat1.getAge()));
+        // Cat cat3 = new Cat( "Robi", "red", 2, false );
+        //     cat3.setFurColor( "striped" );
+        //     output( cat3.getStringAttributes( "#name" ) );
+        //     output( cat3.getStringAttributes( "#furColor" ) );
+        //     output( cat3.getAge() );
+        //     output( "------------------------" );
 
+        Cat cat4 = new Cat( "Lilly", "brown", 5, true );
+            output( cat4.getStringAttributes( "#name" ) );
+            output( cat4.getStringAttributes( "#furColor" ) );
+            output( cat4.getAge() );
+            output( cat4.getAge() );
+            output( cat4.getAge() );
+            output( cat4.getAge() );
+            output( "------------------------" );
     }
 
     // Methode wird aus der KLASSE aufgerufen, daher static
-    public static void output(String outputStr) {
-        System.out.println(outputStr);
-    }
-
-
-
-
+        public static void output( String outputStr ) { System.out.println( outputStr ); }
 }
 
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -5,45 +5,55 @@ public class Cat {
    private String name;
    private String furColor;
    private int age;
+   private int level = 1;
+   private boolean isFemale;
 
-    public Cat(String name, String furColor, int age) {
+    public Cat( String name, String furColor, int age, boolean isFemale ) {
         this.name = name;
         this.furColor = furColor;
         this.age = age;
+        this.isFemale = isFemale;
 	}
 
-    public String getName() {
-        return name;
+    public String getStringAttributes( String flag ) {                                  // zusammengefasster getter
+        switch ( flag ) {
+            case "#name":
+                return name;
+            case "#furColor":
+                return furColor;
+            case "#age":
+                return String.valueOf( age );
+            case "#age2":
+                level++;
+                return checkEscaltionLevel( level );
+            default:
+                return "Error";
+        }
     }
-
-    public void setName(String name) {
-        this.name = name;
+    public String getAge() {                                                            // age als String Ausgabe
+        if ( !isFemale ) {
+            return Integer.toString( age ); 
+        } else {
+            level++;
+            return checkEscaltionLevel( level );
+        }
     }
+    public int getAge2() { return age; }                                                // age als int Ausgabe
 
-    public String getFurColor() {
-        return furColor;
+    public void setName( String name ) { this.name = name; }                            // setter
+    public void setFurColor( String furColor ) { this.furColor = furColor; }            // setter
+    public void setAge( int age ) { this.age = age; }                                   // setter
+
+    public String checkEscaltionLevel( int level ) {
+        switch ( level ) {
+            case 1:
+                return "DARE to ask";
+            case 2:
+                return "Really?";
+            case 3:
+                return "Seriously?";
+            default:
+                return "Some peolples never learn to be decent";
+        }
     }
-
-    public void setFurColor(String furColor) {
-        this.furColor = furColor;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-
-
-    
-
-
-
-    
-
-
-    
 }
